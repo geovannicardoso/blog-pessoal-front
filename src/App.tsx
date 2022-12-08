a49ec2d06ae556a1e54e708dfb17f798bfb194e4
geovanni.cardoso <geovanni.moreira@gmail.com>
2022-12-08 15:57:13 -0300

    Cadastro e usuario

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,27 @@ import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import './App.css';
 import Login from './paginas/login/Login';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 
 function App() {
-    return (
-      <Router>
-        <Navbar />
-        <Routes>
-  
-          <Route path="/" element={<Login  />} />
-  
-          <Route path="/home" element={<Home />} />
-  
-          <Route path="/login" element={<Login />} />
-  
-        </Routes>
-        <Footer />
-      </Router>
-  
-    );
-  }
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+
+        <Route path="/" element={<Login  />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
+
+      </Routes>
+      <Footer />
+    </Router>
+
+  );
+}
 
 export default App;
--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Typography, TextField, Button } from '@material-ui/core';
+import { Grid,Typography, TextField, Button } from '@material-ui/core';
 import {Box} from '@mui/material';
 import { Link } from 'react-router-dom';
 import './Login.css';
@@ -26,7 +26,10 @@ function Login() {
                         <Box marginRight={1}>
                             <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
                         </Box>
+                        <Link to='/cadastrousuario'>
                             <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
+                        </Link>
+                            
                     </Box>
                 </Box>
             </Grid>
